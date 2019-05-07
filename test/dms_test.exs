b0d9a7e307c21ede474f54557ab387b54f4a88bb
Paul McLoughlin <paul.mcloughlin@instil.co>
2019-05-07 19:57:29 +0100

    issue resolved

--- test/dms_test.exs
+++ test/dms_test.exs
@@ -1,5 +1,6 @@
 defmodule DMSTest do
   use ExUnit.Case
+
   doctest DMS
 
   test "ping should init a new dms" do
@@ -8,10 +9,24 @@ defmodule DMSTest do
     assert DMS.alive?(id)
   end
 
+  test "ping twice should return ok" do
+    import Task, only: [async: 1, await: 1]
+    id = "some_id"
+    ping = fn -> DMS.ping(id) end
+
+    resps = 1..10
+    |> Enum.map(fn _ -> Task.async(ping) end)
+    |> Enum.map(fn t -> Task.await(t) end)
+
+    for resp <- resps do
+      assert :pong = resp
+    end
+  end
+
   test "service should die after 5 seconds" do
     id = "test-svc-2"
     assert :pong = DMS.ping(id)
-    Process.sleep(5_000)
+    Process.sleep(5_010)
     refute DMS.alive?(id)
   end
 end
